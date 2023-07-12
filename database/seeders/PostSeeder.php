a5164803e7a74801bd75dde78a8d26be5e0c748a
Luca Lambiase <luca.lambia@gmail.com>
2023-07-12 11:38:38 +0200

    data(seeder): add data for model in relationship

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Post;
@@ -16,11 +17,16 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        $categories = Category::all(["id"]);
+
         for ($i=0; $i < 10; $i++) { 
             $post = new Post();
             $post->title = $faker->words(3, true);
             $post->content = $faker->text(500);
             $post->image = $faker->imageUrl(800, 600, 'animals', true);
+            // $post->category_id = rand(1,4); <--- è hardcoded, no bueno
+            // $post->category_id = rand(1, count($categories) );
+            $post->category_id = $categories->random()->id;
             $post->save();
         }
 
